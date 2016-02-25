8143f530141ef26938b328bd4a3ef310a00c9a2c
Sebastian Wilzbach <seb@wilzba.ch>
2016-02-25 04:55:27 +0200

    font-weight to 300

--- src/functional.d
+++ src/functional.d
@@ -1,3 +1,8 @@
+/*
+Please don't use global imports - every test should be independent.
+*/
+
+// Needed for custom name attribute
 struct name
 {
     string val;
@@ -9,9 +14,8 @@ struct name
     import std.range: enumerate;
     assert([3,5] == [3,4,5].enumerate(0).filter!(a => a[0] != 1).map!"a[1]".array);
 }
-@name("Testing") @safe unittest{
-    import std.algorithm: filter, map;
+@name("Reverse sort") @safe unittest{
+    import std.algorithm: sort;
     import std.array: array;
-    import std.range: enumerate, iota;
-    assert([0,2,3] == iota(4).enumerate(1).filter!(a => a[0] != 2).map!"a[1]".array);
+    assert([3,2,1] == [1,3,2].sort!"a > b".array);
 }
