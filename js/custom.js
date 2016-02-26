126f6100fb4dc94210f73b6f0e814cc621d90220
Sebastian Wilzbach <seb@wilzba.ch>
2016-02-26 21:51:25 +0200

    track active menu element

--- js/custom.js
+++ js/custom.js
@@ -30,4 +30,42 @@ jQuery(function($) {
 
   $('.js-show-sidedrawer').on('click', showSidedrawer);
   $('.js-hide-sidedrawer').on('click', hideSidedrawer);
+
+  // get menu clicks
+    $(function(){
+        $(document).on('click','a',function(){
+            var id = $(this).attr('id');
+            menuItems.parent().removeClass("active")
+            $(this).addClass("active");
+        })
+    })
+    var topMenu = $("#sidedrawer-list"),
+    topOffset = $("header").outerHeight(),
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+    // Bind to scroll
+    $(window).scroll(function(){
+       // Get container scroll position
+       var fromTop = $(this).scrollTop()+topOffset;
+    
+       // Get id of current scroll item
+       var cur = scrollItems.map(function(){
+         if ($(this).offset().top < fromTop)
+           return this;
+       });
+       // Get the id of the current element
+       cur = cur[cur.length-1];
+       var id = cur && cur.length ? cur[0].id : "";
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+    });
+
 });
