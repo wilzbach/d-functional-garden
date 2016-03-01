1a492f6b4c365b4ddcd6c5552051f30937354749
Sebastian Wilzbach <seb@wilzba.ch>
2016-03-01 15:59:37 +0200

    more style improvements for snippets

--- src/functional.d
+++ src/functional.d
@@ -1,6 +1,10 @@
 /*
-Please don't use global imports of the functions you want to show.
-Every test should be independent.
+
+Guidelines:
+    - Please don't use global imports of the functions you want to show.
+        (Every test should be independent.)
+    - Add one line between imports and your tests if you have more than one
+        import line
 */
 
 // we use this comparison method a lot
@@ -22,24 +26,27 @@ With map we can call a custom function for every element
 }
 
 /**
+With reduce we can apply a function with a starting value and call it with
+the memo and the current value for all values.
+*/
+@name("Reduce to minimum") @safe unittest{
+    import std.algorithm: min, max, reduce;
+    auto result = [3, 2, 1].reduce!min;
+    assert(result == 1);
+    result = [3, 2, 1].reduce!max;
+    assert(result == 3);
+}
+
+/**
 We can also filter our input range with custom functions
 */
 @name("Filter elements") @safe unittest{
     import std.algorithm: count, filter;
     import std.string: indexOf;
+
     auto result = ["hello", "world"]
                     .filter!(a => a.indexOf("wo") >= 0)
                     .count;
-    assert(result == 1);
-}
-
-/**
-Alternatively we can also use the 
-*/
-@name("Find minimum") @safe unittest{
-    import std.algorithm: min, minPos, reduce;
-    import std.range: enumerate;
-    auto result = [3, 2, 1].reduce!min;
     assert(result == 1);
 }
 
@@ -48,6 +55,7 @@ Sort accepts a pred template, which means we can just pass our own
 */
 @name("Reverse sort") @safe unittest{
     import std.algorithm: sort;
+
     auto result = [1, 3, 2].sort!"a > b";
     assert(result.equal([3, 2, 1]));
 }
@@ -60,12 +68,14 @@ Splitter is usually lazily evaluated.
     import std.algorithm: map, splitter;
     import std.array: array;
     import std.conv: to;
+
     auto result = "1 3 2".splitter().map!(to!int);
     assert(result.equal([1, 3, 2]));
 }
 
 /**
-Awesome description
+A typical example from Unix is to sort and count the number of unique lines.
+In D we can do the same!
 */
 @name("Count number of unique elements") @safe unittest{
     import std.algorithm: count, sort, uniq;
@@ -79,6 +89,7 @@ We split our input range into chunks of the size two and calculate the sum for e
 @name("Pairwise sum") @safe unittest{
     import std.algorithm: map, sum;
     import std.range: chunks;
+
     auto result = [1, 2, 3, 4].chunks(2).map!(sum);
     assert(result.equal([3, 7]));
 }
@@ -91,8 +102,11 @@ Use a dict (below) - it doesn't require sorting.
     import std.array: array;
     import std.algorithm: sort, group;
     import std.typecons: tuple; // create tuples manually
+
     auto result = "ABCA".array.sort().group.array;
-    auto expected = [tuple('A', 2), tuple('B', 1), tuple('C', 1)];
+    auto expected = [tuple('A', 2),
+                     tuple('B', 1),
+                     tuple('C', 1)];
     assert(expected == cast(typeof(expected)) result);
 }
 
@@ -105,6 +119,7 @@ The syntax to convert a tuple back to list is a bit hard to figure out.
     import std.array: array, join;
     import std.range: dropOne, only, save, zip;
     import std.conv: to;
+
     auto arr = "AGCGA".array;
     auto result = arr.zip(arr.save.dropOne)
                      .map!"a.expand.only"
@@ -123,13 +138,14 @@ The syntax to convert a tuple back to list is a bit hard to figure out.
     import std.algorithm: each, map;
     import std.range: dropOne, only, save, zip;
     import std.conv: to;
+
     int[string] d;
     auto arr = "AGAGA".array;
     arr.zip(arr.save.dropOne)
        .map!"a.expand.only"
        .map!(to!string)
        .each!(a => d[a]++);
-    assert(d == ["AG":2, "GA":2]);
+    assert(d == ["AG": 2, "GA": 2]);
 }
 
 /**
@@ -138,11 +154,12 @@ With enumerate we get an index which we can use to filter
 @name("Filter by index") @safe unittest{
     import std.algorithm: filter, map;
     import std.range: enumerate;
+
     auto result = [3, 4, 5]
                     .enumerate
                     .filter!(a => a[0] != 1)
                     .map!"a[1]";
-    assert(result.equal([3,5]));
+    assert(result.equal([3, 5]));
 }
 
 /**
@@ -151,6 +168,7 @@ With enumerate we get an index with which we can remove all odd numbers.
 @name("Sum up even indexed number") @safe unittest{
     import std.algorithm: filter, map, sum;
     import std.range: enumerate;
+
     auto result = [3, 4, 5]
                     .enumerate
                     .filter!(a => a[0] % 2 == 0)
@@ -167,6 +185,7 @@ Yet another good example of group.
     import std.array: split;
     import std.string: toLower;
     import std.typecons: tuple;
+
     string text = "Two times two equals four";
     auto result = text
                     .toLower
@@ -184,9 +203,10 @@ Also note that the second base case is not necessary.
 @name("Quicksort") @safe unittest{
     import std.algorithm: filter;
     import std.array: array;
+
     int[] qs(int[] arr) {
         if(!arr.length) return [];
-        if(arr.length == 1) return arr;
+        if(arr.length == 1) return arr; // optional
         return qs(arr.filter!(a => a < arr[0]).array) ~ arr[0] ~ qs(arr[1..$].filter!(a => a >= arr[0]).array);
     }
     assert(qs([3, 2, 1, 4]) == [1, 2, 3, 4]);
